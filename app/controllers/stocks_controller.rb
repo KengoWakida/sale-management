1425b8409f2d0f9277a3ce999453e94f554bc705
KengoWakida <wakida.kengo.mac@gmail.com>
2021-10-10 15:13:08 +0900

    在庫詳細、在庫削除の編集

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,4 +3,13 @@ class StocksController < ApplicationController
     @stocks = Stock.all
   end
 
+  def show
+    @stock = Stock.find(params[:id])
+  end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    stock.destroy
+  end
+
 end
