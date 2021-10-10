0b0cc15069264a0af416c25b9712a92542bfe54c
KengoWakida <wakida.kengo.mac@gmail.com>
2021-10-10 23:10:54 +0900

    formオブジェクトの編集

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,10 +8,18 @@ class StocksController < ApplicationController
   end
 
   def update
-    stock = Stock.find(params[:id])
+    @stock = Stock.find(params[:id])
+    @stock.update(stock_params)
+    if @stock.valid?
+      @stock.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+    #stock = Stock.find(params[:id])
     #stock.quantity =  (Stock.find(params[:id])[:quantity]).to_i - (params.require(:stock)[:quantity]).to_i
-    stock.update(stock_params)
-    Sell.create(sell_params)
+    #stock.update(stock_params)
+    #Sell.create(sell_params)
   end
 
   def show
@@ -24,15 +32,18 @@ class StocksController < ApplicationController
   end
 
   private
+    #def stock_params
+    #  binding.pry
+    #  params.require(:stock_sell).permit(:item, :price,:quantity).merge(user_id: current_user.id)
+    #end
+  
     def stock_params
       params.require(:stock).permit(:item, :quantity)
     end
 
-    def sell_params
-      binding.pry
-      params.require(:stock).permit(:item, :quantity, :price).merge(user_id: current_user.id)
-    end
-
+    #def sell_params
+    #  params.require(:stock).permit(:item, :quantity, :price).merge(user_id: current_user.id)
+    #end
 
    # def calc_quantity #在庫の数量計算 入力した値を計算してUPDATEする方法を調べる
    #   quantity =  (Stock.find(params[:id])[:quantity]).to_i - (params.require(:stock)[:quantity]).to_i
