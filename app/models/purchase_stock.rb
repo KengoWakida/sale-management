81378209473fa01ad2558347dd18f6bcdd7f87fd
KengoWakida <wakida.kengo.mac@gmail.com>
2021-10-10 12:42:44 +0900

    Formオブジェクトの編集

---
+++ app/models/purchase_stock.rb
@@ -0,0 +1,18 @@
+class PurchaseStock
+  include ActiveModel::Model
+  attr_accessor :order_no, :item,:quantity, :price, :user_id
+
+  with_options presence: true do
+    validates :order_no,numericality:{only_integer: true}
+    validates :item
+    validates :quantity,numericality:{only_integer: true}
+    validates :price,numericality:{only_integer: true}
+    validates :user_id
+  end
+
+  def save
+    #購入情報を保存し、purchaseに代入する
+    purchase = Purchase.create(order_no: order_no, item: item, quantity: quantity, price: price,user_id: user_id)
+    #在庫情報を保存する
+    Stock.create(item: item, quantity: quantity)
+end
