f87ad02b99e37b178a32f55d345adaba76b2c864
KengoWakida <wakida.kengo.mac@gmail.com>
2021-10-28 20:00:36 +0900

    Userモデルの雛形作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  
+  decsribe 'ユーザー新規登録' do
+    it 'nameとemail、password、passwordconfirmationが存在すれば登録できる' do
 
+    end
+    it 'nameが空では登録できない' do
 
+    end
+    it 'emailが空では登録できない' do
+  
+    end
+    it 'passwordが空では登録できない' do
+
+    end
+    it 'password confirmationが空では登録できない' do
+
+    end
+    it 'passwordとpassword_confirmationが等しく無い場合は登録できない' do
+
+    end
+    it 'passwordが5文字以下の場合登録できない' do
+
+    end
+    it '重複したemailが存在する場合登録できない' do
+
+    end
+  end
 end
